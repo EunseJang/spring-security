aef5be59e055acd72563dc10d2475e89e314ac7e
Eunse Jang <senuej37@gmail.com>
2024-01-12 15:18:28 +0900

    feat : 로그인 DTO 추가

---
+++ src/main/java/spring/security/global/auth/dto/LoginDTO.java
@@ -0,0 +1,15 @@
+package spring.security.global.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDTO {
+    private String email;
+    private String password;
+}
